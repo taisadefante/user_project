261ee549a107e9891c8e2f38c06cca94fac8f15d
tais defante <taisadefante@hotmail.com>
2024-01-07 13:35:33 -0300

    primeiro commit

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import background from "./assets/11.png";
+
+export const Container = styled.div`
+  background: url("${background}");
+  background-size: cover;
+`;
+
+export const Image = styled.img``;
+
+export const ContainerItens = styled.div``;
+
+export const H1 = styled.h1``;
+
+export const InputLabel = styled.p``;
+
+export const Input = styled.input``;
+
+export const Button = styled.button``;
